f4168c10b722a3d98521914f3704cd6960ce63e2
Jairaj Jangle <jairaj.jangle@gmail.com>
2022-09-09 22:35:44 +0530

    chore: added original source upon which this lib is built

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,60 @@
+// Original source: https://stackoverflow.com/a/67368639/7040601
+
 export class Storage {
-    private data: Map<string, any>;
+  private data: Map<string, any>;
 
-    constructor() {
-        this.data = new Map<string, any>();
-    }
+  constructor() {
+    this.data = new Map<string, any>();
+  }
 
-    /**
-     * Get key name by index
-     * @param n index
-     * @returns key
-     */
-    public key(n: number): string | undefined {
-        return [...this.data.keys()][n];
-    }
+  /**
+   * Get key name by index
+   * @param n index
+   * @returns key
+   */
+  public key(n: number): string | undefined {
+    return [...this.data.keys()][n];
+  }
 
-    /**
-     * Get value by key
-     * @param key 
-     * @returns value if present else `undefined`
-     */
-    public getItem(key: string) {
-        return this.data.get(key);
-    }
+  /**
+   * Get value by key
+   * @param key
+   * @returns value if present else `undefined`
+   */
+  public getItem(key: string) {
+    return this.data.get(key);
+  }
 
-    /**
-     * Get how many keys-value pairs are stored
-     */
-    public get length(): number {
-        return this.data.size;
-    }
+  /**
+   * Get how many keys-value pairs are stored
+   */
+  public get length(): number {
+    return this.data.size;
+  }
 
-    /**
-     * Store key-value pair
-     * @param key 
-     * @param value 
-     */
-    public setItem(key: string, value: any) {
-        this.data.set(key, value);
-    }
+  /**
+   * Store key-value pair
+   * @param key
+   * @param value
+   */
+  public setItem(key: string, value: any) {
+    this.data.set(key, value);
+  }
 
-    /**
-     * Removes value by key
-     * @param key 
-     */
-    public removeItem(key: string) {
-        this.data.delete(key);
-    }
+  /**
+   * Removes value by key
+   * @param key
+   */
+  public removeItem(key: string) {
+    this.data.delete(key);
+  }
 
-    /**
-     * Clear all keys
-     */
-    public clear() {
-        this.data = new Map<string, any>();
-    }
-
+  /**
+   * Clear all keys
+   */
+  public clear() {
+    this.data = new Map<string, any>();
+  }
 }
 
 const SessionStorage = new Storage();
