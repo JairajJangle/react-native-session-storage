a1919331bd1eeb848db4da92495a568fbbf2d281
Jairaj Jangle <jairaj.jangle@gmail.com>
2024-06-30 11:13:51 +0530

    feat: ported example app to use expo

---
+++ example/app/index.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView
+} from 'react-native';
+import SessionStorage from 'react-native-session-storage';
+
+export default function App() {
+  const [storedVal, updateStoredVal] = React.useState();
+  const [inputText, setInputText] = React.useState<string>("");
+
+  const setVal = React.useCallback(() => {
+    SessionStorage.setItem("my_key", inputText);
+  }, [inputText]);
+
+  function getVal(): any {
+    const val = SessionStorage.getItem("my_key");
+    updateStoredVal(val);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={styles.title}>Input Value to Store</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setInputText} />
+
+      <View style={styles.padding} />
+
+      <Button
+        color={"red"}
+        title='Set'
+        onPress={setVal} />
+
+      <View style={styles.bigPadding} />
+
+      <Text style={styles.title}>Stored Val:</Text>
+      <Text style={styles.message}>{storedVal}</Text>
+
+      <View style={styles.padding} />
+
+      <Button
+        title='Get'
+        onPress={getVal} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20
+  },
+  title: {
+    alignSelf: "flex-start",
+    fontSize: 18
+  },
+  message: {
+    alignSelf: "flex-start",
+    fontSize: 16,
+
+    borderColor: "grey",
+    borderWidth: 1,
+
+    padding: 5,
+    width: "100%"
+  },
+  textInput: {
+    backgroundColor: "white",
+    fontSize: 16,
+    width: "100%",
+
+    borderColor: "grey",
+    borderWidth: 1,
+
+    padding: 5
+  },
+  padding: {
+    padding: 4,
+  },
+  bigPadding: {
+    padding: 20,
+  }
+});
--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  SessionStorageExample
-//
-
-import Foundation
